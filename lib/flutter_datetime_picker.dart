bf3f2af34f7aa9a39c742ce6777f75eb4ddb0839
爬虫饲养员 <56294698+oooooocean@users.noreply.github.com>
2022-05-31 15:16:46 +0800

    Update flutter_datetime_picker.dart

--- lib/flutter_datetime_picker.dart
+++ lib/flutter_datetime_picker.dart
@@ -309,7 +309,7 @@ class _DatePickerState extends State<_DatePickerComponent> {
               ),
               child: GestureDetector(
                 child: Material(
-                  color: theme.backgroundColor,
+                  color: theme.backgroundColor ?? Colors.white,
                   child: _renderPickerView(theme),
                 ),
               ),
